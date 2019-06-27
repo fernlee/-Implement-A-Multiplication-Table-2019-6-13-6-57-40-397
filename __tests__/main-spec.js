6a009d0b4b6caa78778ee298da29ef9c2512c7f9
yaeli <yaeli@thoughtworks.com>
2019-06-27 17:49:29 +0800

    [Yae][return the multiply table when start number is smaller than or equal to the end number]

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,10 +1,15 @@
 const createMultiplyTable = require('../main');
 
-it ('should not return null when start number is smaller than or equal to the end number', () => {
-    expect(createMultiplyTable(2, 2)).not.toBe(null);
-    expect(createMultiplyTable(2, 3)).not.toBe(null);
-});
-
 it ('should return null when start number is larger than the end number', () => {
     expect(createMultiplyTable(3, 2)).toBe(null);
+});
+
+it ('should return null when start number and the end number out of range from 1 to 1000', () => {
+    expect(createMultiplyTable(0, 1000)).toBe(null);
+    expect(createMultiplyTable(1, 1001)).toBe(null);
+});
+
+it ('should not return multiply table when start number is smaller than or equal to the end number', () => {
+    expect(createMultiplyTable(2, 2)).toBe("2*2=4\t\n");
+    expect(createMultiplyTable(2, 3)).toBe("2*2=4\t\n2*3=6\t3*3=9\t\n");
 });
--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+function isNumberOutRange(number) {
+    return 1 > number || 1000 < number;
+}
+
 function createMultiplyTable(start, end) {
     if (start > end) {
         return null;
     }
-    return 0;
+    if (isNumberOutRange(start) || isNumberOutRange(end)) {
+        return null;
+    }
+    let result = "";
+    for (let i = 0; i <= end-start; i++) {
+        for (let j = start; j <= start+i; j++) {
+            result += `${j}*${start+i}=${(start+i)*j}\t`;
+        }
+        result += "\n";
+    }
+    return result;
 }
 
 module.exports = createMultiplyTable;
